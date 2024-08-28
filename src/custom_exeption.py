f3724722baed80c9400a0ab91f26276de2e12208
AbelAdissu <avelodollasign84@gmail.com>
2024-08-28 19:16:16 +0000

    updating custom exeption file

---
+++ src/custom_exeption.py
@@ -0,0 +1,17 @@
+import sys
+
+class NewsAnalysisException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = self.error_message_detail(error_message, error_detail)
+
+    def error_message_detail(self, error_message, error_detail: sys):
+        _, _, exc_tb = error_detail.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_no = exc_tb.tb_lineno
+
+        error_message_detail = f"Error occurred in file: {file_name} on line number: {line_no}. Error message: {error_message}"
+        return error_message_detail
+
+    def __str__(self):
+        return self.error_message
